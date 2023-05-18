9e3912d459bc8baa1d4de51c4de088f815d33a54
Swift <swiftzephyr17@gmail.com>
2023-05-17 20:10:49 -0700

    Competed Screenshotter

--- .ipynb_checkpoints/frontiersScreenshotter-checkpoint.py
+++ .ipynb_checkpoints/frontiersScreenshotter-checkpoint.py
@@ -1,11 +1,24 @@
 import time
-import psutil
+import multiprocessing
 from PIL import ImageGrab
 
-bounds = (440, 0, 3000, 1440)
-count = 0
-while "SonicFrontiers.exe" in (i.name() for i in psutil.process_iter()):
-	screenshot = ImageGrab.grab(bounds)
-	screenshot.save("E:\\Sonic-Frontiers-Dataset\\SonicFrontiers" + str(count) + ".jpg")
-	count += 1
-	time.sleep(1)
+def takeScreenshot():
+	bounds = (0, 143, 2736, 1683)
+	count = 0
+	time.sleep(5)
+	
+	while True:
+		screenshot = ImageGrab.grab(bounds)
+		screenshot = screenshot.resize((1333, 800))
+		screenshot.save("D:\\Sonic-Frontiers-Dataset\\SonicFrontiers (" + str(count) + ").jpg")
+		count += 1
+		time.sleep(1)
+
+if __name__ == '__main__': 
+	function = multiprocessing.Process(target = takeScreenshot, name = "TakeScreenshot")
+	function.start()
+
+	time.sleep(31163)
+
+	function.terminate()
+	function.join()
